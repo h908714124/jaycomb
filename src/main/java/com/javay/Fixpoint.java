3a71ef44b16670ec0b29d0febbbcbaff9e4fc339
h908714124 <kraftdurchblumen@gmx.de>
2020-05-02 21:18:52 +0200

    refactoring

--- src/main/java/com/javay/Fixpoint.java
+++ src/main/java/com/javay/Fixpoint.java
@@ -5,15 +5,15 @@ import java.util.function.Function;
 
 class Fixpoint<T, R> implements Function<T, R> {
 
-    private Function<T, R> fixpoint;
+    private Function<T, R> f;
 
     // Create a recursive version of r.
     Fixpoint(BiFunction<Function<T, R>, T, R> r) {
-        this.fixpoint = t -> r.apply(fixpoint, t);
+        f = t -> r.apply(f, t);
     }
 
     @Override
     public R apply(T t) {
-        return fixpoint.apply(t);
+        return f.apply(t);
     }
 }
