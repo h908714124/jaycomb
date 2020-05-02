0253e4f2e7097f27d3b2a3dc15d3ec5a8a7e3564
h908714124 <kraftdurchblumen@gmx.de>
2020-05-02 15:55:21 +0200

    refactoring

--- src/main/java/com/javay/Fixpoint.java
+++ src/main/java/com/javay/Fixpoint.java
@@ -6,18 +6,16 @@ import java.util.function.UnaryOperator;
 
 class Fixpoint {
 
-    // Creates a fixpoint.
-    private static <T, R> Function<UnaryOperator<Function<T, R>>, Function<T, R>> create() {
-        return r -> {
-            SelfApply<T, R> self1 = SelfApply.createEager();
-            SelfApply<T, R> self2 = SelfApply.createLazy(r);
-            return self1.apply(self2);
-        };
+    // Creates a fixpoint of r.
+    private static <T, R> Function<T, R> create(UnaryOperator<Function<T, R>> r) {
+        SelfApply<T, R> f = SelfApply.create(r);
+        // (λf.(r λx.((f f) x)) λf.(r λx.((f f) x)
+        return f.apply(f);
     }
 
     // Convenient recursifier, but note that the resulting function can overflow the stack.
     static <T, R> Function<T, R> recursify(BiFunction<Function<T, R>, T, R> r) {
         UnaryOperator<Function<T, R>> operator = f -> t -> r.apply(f, t);
-        return Fixpoint.<T, R>create().apply(operator);
+        return Fixpoint.create(operator);
     }
 }
--- src/main/java/com/javay/SelfApply.java
+++ src/main/java/com/javay/SelfApply.java
@@ -8,13 +8,8 @@ interface SelfApply<T, R> {
     // Self referential method, a necessary evil?
     Function<T, R> apply(SelfApply<T, R> x);
 
-    // λf.(f f)
-    static <T, R> SelfApply<T, R> createEager() {
-        return f -> f.apply(f);
-    }
-
     // λf.(r λx.((f f) x)
-    static <T, R> SelfApply<T, R> createLazy(UnaryOperator<Function<T, R>> r) {
+    static <T, R> SelfApply<T, R> create(UnaryOperator<Function<T, R>> r) {
         return f -> {
             Function<T, R> lazy = x -> {
                 Function<T, R> eager = f.apply(f);
