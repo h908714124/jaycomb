353407167a1d1976a2fa69e7da667fb6bda95c67
h908714124 <kraftdurchblumen@gmx.de>
2020-05-03 13:52:25 +0200

    update

--- src/test/java/com/javay/FactorialTest.java
+++ src/test/java/com/javay/FactorialTest.java
@@ -11,14 +11,14 @@ import static java.math.BigInteger.ZERO;
 
 class FactorialTest {
 
-    BigInteger rfact(Function<BigInteger, BigInteger> f, BigInteger n) {
+    private BigInteger rfact(Function<BigInteger, BigInteger> f, BigInteger n) {
         if (n.equals(ZERO)) {
             return ONE;
         }
         return n.multiply(f.apply(n.subtract(ONE)));
     }
 
-    Function<BigInteger, BigInteger> fact = new Fixpoint<>(this::rfact);
+    private Function<BigInteger, BigInteger> fact = new Fixpoint<>(this::rfact);
 
     @Test
     void testFact() {
