e22a50cb771019fb10b1ea21f4d06ff5dae9c6d4
h908714124 <kraftdurchblumen@gmx.de>
2020-05-03 14:47:00 +0200

    update

--- src/test/java/com/javay/AdditionTest.java
+++ src/test/java/com/javay/AdditionTest.java
@@ -13,18 +13,20 @@ import static java.math.BigInteger.ZERO;
 
 class AdditionTest {
 
-    private BigInteger radd(Function<Entry<BigInteger, BigInteger>, BigInteger> f, Entry<BigInteger, BigInteger> n) {
-        if (n.getKey().equals(ZERO)) {
-            return n.getValue();
+    private BigInteger radd(Function<Entry<BigInteger, BigInteger>, BigInteger> f, Entry<BigInteger, BigInteger> p) {
+        BigInteger n1 = p.getKey();
+        BigInteger n2 = p.getValue();
+        if (n2.equals(ZERO)) {
+            return n1;
         }
-        return f.apply(pair(n.getKey().subtract(ONE), n.getValue().add(ONE)));
+        return f.apply(pair(n1.add(ONE), n2.subtract(ONE)));
     }
 
     private Function<Entry<BigInteger, BigInteger>, BigInteger> add = new Fixpoint<>(this::radd);
 
     @Test
     void testAdd() {
-        Assertions.assertEquals(BigInteger.valueOf(4), add.apply(pair(11, -7)));
+        Assertions.assertEquals(BigInteger.valueOf(4), add.apply(pair(-7, 11)));
         Assertions.assertEquals(BigInteger.valueOf(10), add.apply(pair(1, 9)));
     }
 
